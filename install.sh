164f40ef4d0f279a32a5866b85ac9520b425fc29
Tonkovski <tkfski@gmail.com>
2025-03-24 14:33:08 +0800

    a little makeup

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
+#!/usr/bin/env bash
+
+SH_PATH=${DAT_PATH:-/usr/local/share/seuconnect}
+
+JSON_PATH=${JSON_PATH:-/usr/local/etc/seuconnect}
 check_if_running_as_root() {
-  # If you want to run as another user, please modify $UID to be owned by this user
   if [[ "$UID" -ne '0' ]]; then
-    echo "WARNING: The user currently executing this script is not root. You may encounter the insufficient privilege error."
-    read -r -p "Are you sure you want to continue? [y/n] " cont_without_been_root
-    if [[ x"${cont_without_been_root:0:1}" = x'y' ]]; then
-      echo "Continuing the installation with current user..."
-    else
-      echo "Not running with root, exiting..."
-      exit 1
-    fi
+    echo "Not running with root, exiting..."
+    exit 1
   fi
 }
